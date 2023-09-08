650958f1017d99d8ffc2196ebe1aabef3cc5d168
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2023-09-08 18:42:10 +0800

    learn user input and pointers

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main(){
 	eventName := "Tayangan Perdana Fast Furios 18: The Family" // eventName := is equal to var eventName string =
-	const numberOfTickets int = 50
+	const numberOfTickets int = 50 // fixed number
 	var remainingTickets uint = 50 // remaining ticket can't be negative
 
 	fmt.Printf("the data type for eventName is %T, for numberOfTickets is %T, for remainingTickets is %T\n",eventName,numberOfTickets,remainingTickets)
@@ -13,11 +13,20 @@ func main(){
 	fmt.Printf("We have only %v tickets and the the remaining number of tickets is %v\n",numberOfTickets, remainingTickets)
 	fmt.Println("Book your ticket now, don't miss this awesome movie!")
 
-	var userName string
+	var userFirstName string
+	var userLastName string
+	var userEmail string
 	var userTickets int
 
-	userName = "Luqman"
-	userTickets = 3
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&userFirstName)
+	fmt.Println("Please enter your second name:")
+	fmt.Scan(&userLastName)
+	fmt.Println("Please enter your email:")
+	fmt.Scan(&userEmail)
+	fmt.Println("Number of tickets you want to purchase")
+	fmt.Scan(&userTickets)
 
-	fmt.Printf("%v just bought %v tickets just now\n", userName, userTickets)
+	fmt.Printf("Hello %v %v and welcome to our booking application!\n", userFirstName,userLastName)
+	fmt.Printf("You (%v) just bought %v tickets.\n", userEmail, userTickets)
 }
